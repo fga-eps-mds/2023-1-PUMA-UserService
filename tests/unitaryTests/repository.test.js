6ed7f4187d2ad738441b36575f5d83d648f03d96
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-11-05 03:22:46 -0300

    Adding tests and constants file
    Co-authored-by: Eduardo Lima <eduardolima.df@gmail.com>
    Co-authored-by: jpmartins201 <jpmartins201@live.com>

--- tests/unitaryTests/repository.test.js
+++ tests/unitaryTests/repository.test.js
@@ -1,28 +1,38 @@
 const assert = require('assert');
 const repository = require('../../src/repository/userRepository');
-// const app = require('../../src/index')
 
-let userId;
-const newUser = {
-  name: 'foo',
-  email: 'foo@bar.com',
-};
+const constants = require('../utils/constants');
+
+const success = constants.success;
+let professorId;
+let studentId;
+let agentId;
 
 describe('addUser', () => {
 
-  const hash = 'ilawefybabgveouyvbfev';
-
-  const newUserFail = {}
+  it('should create a user', () => {
+    repository.addUser(success.newUserProfessor, success.hash).then((response) => {
+      professorId = response;
+      assert.equal(typeof (response), 'number');
+    });
+  });
 
   it('should create a user', () => {
-    repository.addUser(newUser,hash).then((response) => {
-      userId = response;
+    repository.addUser(success.newUserStudent, success.hash).then((response) => {
+      studentId = response;
+      assert.equal(typeof (response), 'number');
+    });
+  });
+
+  it('should create a user', () => {
+    repository.addUser(success.newUserAgent, success.hash).then((response) => {
+      agentId = response;
       assert.equal(typeof (response), 'number');
     });
   });
 
   it('should not create a user', () => {
-    repository.addUser(newUserFail, hash).then((response) => {
+    repository.addUser({}, success.hash).then((response) => {
       assert.equal(response, 'ERROR');
     });
   });
@@ -33,7 +43,27 @@ describe('addUser', () => {
 describe('addProfessor()', () => {
 
   it('should create a professor', () => {
-    repository.addProfessor(userId, newUser).then((response) => {
+    repository.addProfessor(professorId, success.newUserProfessor).then((response) => {
+      assert.equal(typeof (response), 'number');
+    });
+  });
+
+});
+
+describe('addStudent()', () => {
+
+  it('should create a student', () => {
+    repository.addStudent(studentId, success.newUserStudent).then((response) => {
+      assert.equal(typeof (response), 'number');
+    });
+  });
+
+});
+
+describe('addJuridicalAgent()', () => {
+
+  it('should create a juridical agent', () => {
+    repository.addJuridicalAgent(agentId, success.newUserAgent).then((response) => {
       assert.equal(typeof (response), 'number');
     });
   });
---
+++ tests/utils/constants.js
@@ -0,0 +1,17 @@
+module.exports = {
+  success: {
+    hash: 'ilawefybabgveouyvbfev',
+    newUserProfessor: {
+      name: 'foo',
+      email: 'foo@bar.com',
+    },
+    newUserStudent: {
+      name: 'hehe',
+      email: 'student@email.com'
+    },
+    newUserAgent: {
+      name: 'agent',
+      email: 'agent@email.com'
+    },
+  },
+}
