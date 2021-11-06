1e36e62e4fb93bb68402fbf2698454937c172c2a
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-11-05 23:24:09 -0300

    Adding test files

--- tests/utils/constants.js
+++ tests/utils/constants.js
@@ -4,14 +4,46 @@ module.exports = {
     newUserProfessor: {
       name: 'foo',
       email: 'foo@bar.com',
+      matricula: 123456,
     },
     newUserStudent: {
       name: 'hehe',
-      email: 'student@email.com'
+      email: 'student@email.com',
+      matricula: 765442,
     },
-    newUserAgent: {
+    newUserJuridical: {
       name: 'agent',
-      email: 'agent@email.com'
+      email: 'agent@email.com',
+      cnpj: 56854641,
+      cep: 6546546,
+      companyName: 'foo bar',
+      socialReason: 'rensgans'
+    },
+    newUserPhysical: {
+      name: 'physical',
+      email: 'physical@email.com',
+      cpf: 5346854648
+    },
+  },
+  fail: {
+    newUserProfessor: {
+      name: 'foo',
+      email: 'foo@bar.com',
+    },
+    newUserStudent: {
+      name: 'hehe',
+      email: 'student@email.com',
+    },
+    newUserJuridical: {
+      name: 'agent',
+      email: 'agent@email.com',
+      cep: 6546546,
+      companyName: 'foo bar',
+      socialReason: 'rensgans'
+    },
+    newUserPhysical: {
+      name: 'physical',
+      email: 'physical@email.com'
     },
   },
 }
