a2a18f48cc1c1c3a12e5344402044cbe064e8bf2
Eugenio Sales <eugenio.sales@outlook.com>
2022-04-25 22:14:58 -0300

    Atualizar rota

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -24,7 +24,7 @@ module.exports = {
         address: receiverAddress,
       },
       subject: 'Puma - Recuperação de Senha',
-      html: "<h1> Clique no link para recuperar sua senha: <a href='https://puma-2021-2.netlify.app/usuario/newPassword'>Puma</a> <h1>",
+      html: "<h1> Clique no link para recuperar sua senha: <a href='https://puma-2021-2.netlify.app/usuario/atualizar-senha'>Puma</a> <h1>",
     });
 
     return info;
