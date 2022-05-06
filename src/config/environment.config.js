667d53db02b692b56b3032082c7a9b89c3908bc8
Mexazonic <brunnohenriq697@gmail.com>
2022-05-06 02:40:26 -0300

    Correção de strings de ambientes

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 module.exports = {
   configEnv: () => {
     if (process.env.ENVIRONMENT === 'dev') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL;
     } else if (process.env.ENVIRONMENT === 'test') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL_TEST;
     } else if (process.env.ENVIRONMENT === 'hom') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:${process.env.PORT};
+      global.URL_API = `http://${process.env.IP_ADDRESS}:${process.env.PORT}`;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL;
     } else if (process.env.ENVIRONMENT === 'prod') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET_PROD;
       global.DB_URL = process.env.DB_URL;
     }
