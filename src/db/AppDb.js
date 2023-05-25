2241b13209c276478606834ab07c93f531f5200d
Abner Filipe <abner.f.c.r@hotmail.com>
2023-05-25 15:03:24 -0300

    hotfix: Changing way to instantiate database at homolog environment

--- src/db/AppDb.js
+++ src/db/AppDb.js
@@ -2,10 +2,27 @@ const { Sequelize } = require('sequelize');
 
 const appDbConfig = require('../config/appDbConfig');
 
-const sequelize = new Sequelize(appDbConfig.database, appDbConfig.username, appDbConfig.password, {
-    host: appDbConfig.host,
-    dialect: appDbConfig.dialect,
-    query:{raw:true}
-});
+let sequelize;
+
+if (process.env.ENVIRONMENT === 'hom') {
+    sequelize = new Sequelize(appDbConfig.database, appDbConfig.username, appDbConfig.password, {
+        host: appDbConfig.host,
+        dialect: appDbConfig.dialect,
+        query:{raw:true},
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false
+            }
+        }
+    });
+}else {
+    sequelize = new Sequelize(appDbConfig.database, appDbConfig.username, appDbConfig.password, {
+        host: appDbConfig.host,
+        dialect: appDbConfig.dialect,
+        query:{raw:true},
+    });
+}
+
 
 module.exports = sequelize;
