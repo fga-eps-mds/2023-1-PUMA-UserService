938556a13266b45f625a66649ca53999e5f2e3d1
Rafaelltm <rafaelltm10@hotmail.com>
2023-05-29 20:23:18 -0300

    hotfix: fixing sonar issues

--- src/controller/contactController.js
+++ src/controller/contactController.js
@@ -1,40 +1,35 @@
 const contactRepository = require('../repository/contactRepository');
 
 module.exports = {
-    createContact: (newContact) => new Promise( async (resolve, reject) => {
-        try {
-            const contactId = await contactRepository.addContact(newContact);
-            resolve(contactId);
-        } catch (error) {
-            reject(error);
-        }
+    createContact: (newContact) => new Promise((resolve, reject) => {
+        contactRepository.addContact(newContact).then((response) => {
+            resolve(response);
+        }).catch((response) => {
+            reject(response);
+        });
     }),
 
-    getContacts: () => new Promise ( async (resolve, reject) => {
-        try {
-            const contacts = await contactRepository.getContacts();
-            resolve(contacts);
-        } catch (error) {
-            reject(error);
-        }
+    getContacts: () => new Promise((resolve, reject) => {
+        contactRepository.getContacts().then((response) => {
+            resolve(response);
+        }).catch((response) => {
+            reject(response);
+        });
     }),
 
-    updateContact: (contactId, newContact) => new Promise (async (resolve, reject) => {
-        try {
-            const contact = await contactRepository.updateContact(contactId, newContact);
-            resolve(contact);
-        } catch (error) {
-            console.log(error);
-            reject(error);
-        }
+    updateContact: (contactId, newContact) => new Promise((resolve, reject) => {
+        contactRepository.updateContact(contactId, newContact).then((response) => {
+            resolve(response);
+        }).catch((response) => {
+            reject(response);
+        });
     }),
 
-    deleteContact: (contactId) => new Promise (async (resolve, reject) => {
-        try {
-            const contact = await contactRepository.deleteContacts(contactId);
-            resolve(contact);
-        } catch (error) {
-            reject(error);
-        }
+    deleteContact: (contactId) => new Promise((resolve, reject) => {
+        contactRepository.deleteContacts(contactId).then((response) => {
+            resolve(response);
+        }).catch((response) => {
+            reject(response);
+        });
     }),
 };
