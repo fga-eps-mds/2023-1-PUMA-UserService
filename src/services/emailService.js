6848552d9c6b9d975e806a39dff8d2667fcdf44e
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2023-05-28 23:41:09 -0300

    feat: adicionado os emails para envio, mas ainda não colocados nas suas devidas barras, sera adicionada mais a frente

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -92,8 +92,101 @@ module.exports = {
       //html: "<h1> Clique no link para recuperar sua senha: <a href='http://localhost:8080/usuario/atualizar-senha/'>RECUPERAR SENHA</a> <h1>",
 
       //use abaixo em produção
-      html: "<h1> Olá " + userName + ", seja bem-vindo a plataforma PUMA!!!</h1><p>Cadastro efetuado no dia " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "</p>",
+      html: "<h1> Olá " + userName + ", seja bem-vindo a plataforma PUMA!!!<h1><p>Cadastro efetuado no dia " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "</p>",
     });
+
+    return info;
+  },
+
+  async sendEmailToTecaherAcepted(mailerAddress, receiverAddress) {
+    const settings = {
+      service: process.env.EMAIL_SERVICE,
+      port: parseInt(process.env.EMAIL_PORT, 10),
+      auth: {
+        user: process.env.GMAIL_ACCOUNT,
+        pass: process.env.GMAIL_APP_PASS,
+      },
+    };
+
+    const transporter = nodemailer.createTransport(settings);
+
+    const info = await transporter.sendMail({
+      from: {
+        name: 'Puma',
+        address: mailerAddress,
+      },
+      to: {
+        name: 'Usuário',
+        address: receiverAddress,
+      },
+      subject: 'Puma - Cadastro aprovado',
+      //Email a ser enviado para o professor depois do feedback de aprovação do programa
+      //Mensagem reproduzida pelo usuário
+      html: `<h1> Obrigado por fazer seu cadastro, seu status agora é de professor ativo </h1>`,
+    });
+    
+
+    return info;
+  },
+
+  async sendEmailToTecaherRejected(mailerAddress, receiverAddress) {
+    const settings = {
+      service: process.env.EMAIL_SERVICE,
+      port: parseInt(process.env.EMAIL_PORT, 10),
+      auth: {
+        user: process.env.GMAIL_ACCOUNT,
+        pass: process.env.GMAIL_APP_PASS,
+      },
+    };
+
+    const transporter = nodemailer.createTransport(settings);
+
+    const info = await transporter.sendMail({
+      from: {
+        name: 'Puma',
+        address: mailerAddress,
+      },
+      to: {
+        name: 'Usuário',
+        address: receiverAddress,
+      },
+      subject: 'Puma - Cadastro rejeitado',
+      //Email a ser enviado para o professor depois do feedback de aprovação do programa
+      //Mensagem reproduzida pelo usuário
+      html: `<h1> Desculpe, mas por algum motivo seu cadastro foi negado em nossa plataforma, entre em contato com os responsáveis <h1>`,
+    });
+    
+
+    return info;
+  },
+
+  async sendEmailtoAdiminForTeacherAcepted(mailerAddress, receiverAddress) {
+    const settings = {
+      service: process.env.EMAIL_SERVICE,
+      port: parseInt(process.env.EMAIL_PORT, 10),
+      auth: {
+        user: process.env.GMAIL_ACCOUNT,
+        pass: process.env.GMAIL_APP_PASS,
+      },
+    };
+
+    const transporter = nodemailer.createTransport(settings);
+
+    const info = await transporter.sendMail({
+      from: {
+        name: 'Puma',
+        address: mailerAddress,
+      },
+      to: {
+        name: 'Usuário',
+        address: receiverAddress,
+      },
+      subject: 'Puma - Professor aceito',
+      //Email a ser enviado para o professor depois do feedback de aprovação do programa
+      //Mensagem reproduzida pelo usuário
+      html: `<h1> Temos mais um professor cadastrado na nossa plataforma. <h1>`,
+    });
+    
 
     return info;
   },
