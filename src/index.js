f13ab28b39539a75baefe12359a89f45a036fecc
Ítalo Alves <italooko@gmail.com>
2023-06-05 21:31:53 -0300

    feat: expand json and url encoded limit

--- src/index.js
+++ src/index.js
@@ -40,8 +40,8 @@ const corsOptions = {
 // console.log('Api URL:', global.URL_API);
 app.use(cors(corsOptions));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '4mb' }));
+app.use(express.urlencoded({ extended: true, limit: '4mb' }));
 
 require('./routes/router')(app);
 
