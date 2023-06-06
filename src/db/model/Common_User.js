c20722b501ffc2275fcd333d6bc07ca2961da31d
Ítalo Alves <italooko@gmail.com>
2023-06-05 22:08:07 -0300

    feat: add image to common user

--- src/db/model/Common_User.js
+++ src/db/model/Common_User.js
@@ -29,6 +29,10 @@ const Common_User = database.define('Common_User', {
         type: DataTypes.STRING,
         allowNull: false
     },
+    image: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
 }, {
     freezeTableName: true
 })
